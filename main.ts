a13370358c514788e60aafdf5abfbad526c5d5ef
shakao-test <61802990+shakao-test@users.noreply.github.com>
2021-03-25 08:51:39 -0700

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,27 @@
+namespace SpriteKind {
+    export const Camera = SpriteKind.create()
+    //% isKind
+    export const Penguin = SpriteKind.create()
+}
+let __camera = sprites.create(image.create(150, 110), SpriteKind.Camera)
+__camera.image.setPixel(0, 0, 4)
+__camera.image.setPixel(149, 0, 4)
+__camera.image.setPixel(0, 109, 4)
+__camera.image.setPixel(149, 109, 4)
+controller.moveSprite(__camera)
+__camera.setFlag(SpriteFlag.Invisible, true)
+__camera.setFlag(SpriteFlag.Ghost, true)
+__camera.setFlag(SpriteFlag.StayInScreen, true)
+scene.cameraFollowSprite(__camera)
 
+
+let won = false
+scene.onOverlapTile(SpriteKind.Penguin, assets.tile`tile`, function (sprite, location) {
+    if (!(won)) {
+        won = true
+        control.runInParallel(function() { 
+            pause(1500)
+            game.over(true)
+        })
+    }
+})

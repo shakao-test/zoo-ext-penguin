0d203dff48035c705f4d46d203c40acde4159f9a
shakao-test <61802990+shakao-test@users.noreply.github.com>
2021-03-25 09:01:28 -0700

    Update pxt.json, camera.ts

---
+++ camera.ts
@@ -0,0 +1,16 @@
+namespace SpriteKind {
+    export const Camera = SpriteKind.create()
+    //% isKind
+    export const Penguin = SpriteKind.create()
+}
+let __camera = sprites.create(image.create(150, 110), SpriteKind.Camera)
+__camera.image.setPixel(0, 0, 4)
+__camera.image.setPixel(149, 0, 4)
+__camera.image.setPixel(0, 109, 4)
+__camera.image.setPixel(149, 109, 4)
+controller.moveSprite(__camera)
+__camera.setFlag(SpriteFlag.Invisible, true)
+__camera.setFlag(SpriteFlag.Ghost, true)
+__camera.setFlag(SpriteFlag.StayInScreen, true)
+scene.cameraFollowSprite(__camera)
+
